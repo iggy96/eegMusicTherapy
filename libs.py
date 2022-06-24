bd33eb6bacdf4508a8db52e7acd3b853f26d281d
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-06-23 23:11:39 -0700

    Update libs.py

--- libs.py
+++ libs.py
@@ -5,7 +5,7 @@ import zipfile, os
 from zipfile import ZipFile
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
+from scipy import signal,stats
 from scipy.signal import butter,lfilter,sosfiltfilt
 from scipy.signal import spectrogram, welch
 from sklearn.decomposition import FastICA
