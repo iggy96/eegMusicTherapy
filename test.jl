bde0283f0eebd801b853d295d415c8068b557368
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-06-30 09:25:35 -0700

    Delete test.jl

--- test.jl
+++
@@ -1,9 +0,0 @@
-using Pkg
-Pkg.add("MySQL")       # No need for a full PackageSpec here
-Pkg.add("DBInterface")
-
-using MySQL
-using DBInterface
-conn = DBInterface.connect(MySQL.Connection, "ec2blah.eu-west-2.compute.amazonaws.com",
-                           "name", "password", db = "database")
-
