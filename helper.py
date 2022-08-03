9297acee2807ce5e6317422a1b0c2b47400d9a1c
Joshua Ighalo <70718280+iggy96@users.noreply.github.com>
2022-08-03 14:11:32 -0700

    Update helper.py

--- helper.py
+++ helper.py
@@ -1,65 +1,6 @@
 from statistics import mode
 from libs import*
 
-def create_db_connection(host_name, user_name, user_password, database_name):
-    # used to establish created database connection
-    connection = None
-    try:
-        connection = mysql.connector.connect(
-            host=host_name,
-            user=user_name,
-            passwd=user_password,
-            database=database_name
-        )
-        print("MySQL Database connection successful")
-    except Error as err:
-        print(f"Error: '{err}'")
-    return connection
-
-def df_to_table_query(connection, query):
-    # converts all sql queries user writes in python strings 
-    # and passes it to cursor.execute() method to execute them
-    # on the MYSQL server 
-    cursor = connection.cursor()
-    try:
-        df = pd.read_sql(query, con=connection)
-        print("Query successful")
-    except Error as err:
-        print(f"Error: '{err}'")
-    return df
-
-def sqlTableToDataframe(host_name,user_name,user_password,database_name,query):
-    dbConnection = create_db_connection(host_name,user_name,user_password,database_name)
-    sql_query = pd.read_sql_query (query,dbConnection)
-    df = pd.DataFrame(sql_query)
-    return df
-
-def allSQLTableNames(hostName,userName,userPassword,databaseName):
-    # Input: hostName,userName,userPassword,databaseName
-    # Output: list of all table names in database
-    db_connection = create_db_connection(hostName,userName,userPassword,databaseName)
-    cursor = db_connection.cursor()
-    cursor.execute("Show tables;")
-    result = cursor.fetchall()
-    result = [x[0] for x in result]
-    return result
-
-def multiSQLTablesToDataframes(hostName,userName,userPassword,databaseName,table_name):
-    # Input: hostName,userName,userPassword,databaseName,table_name
-    #        table_name is a list of all table names in database
-    # Output: 2D array holding four channel tables
-    def tableToDF(host_name,user_name,user_password,database_name,table_name):
-        query = ("% s % s"%('SELECT * FROM', table_name))
-        dbConnection = create_db_connection(host_name,user_name,user_password,database_name)
-        sql_query = pd.read_sql_query (query,dbConnection)
-        df = pd.DataFrame(sql_query)
-        return df
-    tables_ = []
-    for i in range(len(table_name)):
-        tables_.append(tableToDF(hostName,userName,userPassword,databaseName,table_name[i]))
-    #tables_ = np.array(tables_,dtype=object)
-    return tables_
-
 def singleTransformToRawEEG(data,fs,collection_time,fs_setting):
     #   Inputs  :   data    - one dataframe of unfiltered EEG data
     #   upsampling is common for muse eeg if the custom setting is utilized
@@ -90,97 +31,52 @@ def singleTransformToRawEEG(data,fs,collection_time,fs_setting):
                 break
     return rawEEG,time_s
 
-def multiTransformTableToRawEEG(data,fs,collection_time,fs_setting):
-    #   Inputs  :   data    -multiple dataframes of unfiltered EEG data
-    #   upsampling is common for muse eeg if the custom setting is utilized
-    #   fs = desired sampling frequency
-    #   'constant':eeg signals generated at this rate is perfect
-    #   Outputs  :   rawEEG  - multiple 2D arrays of raw EEG data collapsed in a 3D array
-    newRawEEG = []
-    for i in range(len(data)):
-        newRawEEG.append((singleTransformToRawEEG(data[i],fs,collection_time,fs_setting))[0])
-    newRawEEG = np.dstack(newRawEEG)
-    newRawEEG = newRawEEG.reshape(newRawEEG.shape[2],newRawEEG.shape[0],newRawEEG.shape[1])
-    time_s = singleTransformToRawEEG(data[0],fs,collection_time,fs_setting)[1]
-    return newRawEEG,time_s
+def zipExtract(filenames,localDirectory,destDirectory,variableName,sFreq,data_collection_time,sampling_state):
+    #print("\n")
+    #print("zip files in "+ variableName+":")
+    def zipExt(filename,localDirectory,destDirectory,variableName):
+        zf = ZipFile(localDirectory+filename, 'r')
+        path = destDirectory+variableName
+        zf.extractall(path)
+        zf.close()
+    zipOutputs = []
+    for filename in filenames:
+        zipOutputs.append(zipExt(filename,localDirectory,destDirectory,variableName))
+        #print(filename)
+    files_dest = next(walk(destDirectory+variableName), (None, None, []))[2]
+    files_dest = [f for f in files_dest if f.endswith('.csv')]
+    resampled_data = []
+    resampled_time = []
+    for I in range(len(files_dest)):
+        resampled_data.append(singleTransformToRawEEG((pd.read_csv(destDirectory+variableName+'/'+files_dest[I],low_memory=False))[['RAW_TP9','RAW_AF7','RAW_AF8','RAW_TP10']],sFreq,data_collection_time,sampling_state)[0])
+        resampled_time.append(singleTransformToRawEEG((pd.read_csv(destDirectory+variableName+'/'+files_dest[I],low_memory=False))[['RAW_TP9','RAW_AF7','RAW_AF8','RAW_TP10']],sFreq,data_collection_time,sampling_state)[1])
+    resampled_data = np.array(resampled_data)
+    resampled_time = np.array(resampled_time)
+    #print("\n")
+    #print(variableName + " zip files contents:")
+    #print(files_dest)
+    return files_dest,resampled_data,resampled_time
 
-def multiChannelDWT(data,time_array,wavelet):
-    """
-        Inputs  :   data    -multiple dataframes of unfiltered EEG data
-        upsampling is common for muse eeg if the custom setting is utilized
-        fs = desired sampling frequency
-        'constant':eeg signals generated at this rate is perfect
-        Outputs  :   rawEEG  - multiple 2D arrays of raw EEG data collapsed in a 3D array
-    """
-    dwtEEG = []
+def avgBandPower(data,fs,low,high):
+    #  Inputs  :   data    - 2D numpy array (d0 = samples, d1 = channels) of filtered EEG data
+    #              or data - 3D numpy array (d0 = channels, d1 = no of windows, d2 = length of windows) of unfiltered EEG data
+    #              fs      - sampling rate of hardware (defaults to config)
+    #              low     - lower limit in Hz for the brain wave
+    #              high    - upper limit in Hz for the brain wave
+    #              win     - size of window to be used for sliding
+    #   Output  :   3D array (columns of array,no of windows,window size)
+    def absPower(data,fs,low,high): 
+        win = 4*fs                                                
+        freqs, psd = signal.welch(data,fs,nperseg=win)
+        idx_freqBands = np.logical_and(freqs >= low, freqs <= high) 
+        freq_res = freqs[1] - freqs[0]                                  
+        freqBand_power = simps(psd[idx_freqBands],dx=freq_res)  
+        return freqBand_power
+    avg_BandPower = []
     for i in range(len(data.T)):
-        dwtEEG.append(singleChannelDWT(data[:,i],time_array,wavelet))
-    dwtEEG = np.array(dwtEEG).T
-    dwtEEG = dwtEEG.reshape(dwtEEG.shape[1],dwtEEG.shape[2])
-    return dwtEEG
-
-def plot_averageBandPower(groupA,groupB,x_labels,groups,figure_size,plot_title):
-    mean_groupA_11 = np.mean(groupA[0][0],axis=0)
-    mean_groupA_12 = np.mean(groupA[0][1],axis=0)
-    mean_groupA_21 = np.mean(groupA[1][0],axis=0)
-    mean_groupA_22 = np.mean(groupA[1][1],axis=0)
-    mean_groupB_11 = np.mean(groupB[0][0],axis=0)
-    mean_groupB_12 = np.mean(groupB[0][1],axis=0)
-    mean_groupB_21 = np.mean(groupB[1][0],axis=0)
-    mean_groupB_22 = np.mean(groupB[1][1],axis=0)
-    std_groupA_11 = np.std(groupA[0][0],axis=0)
-    std_groupA_12 = np.std(groupA[0][1],axis=0)
-    std_groupA_21 = np.std(groupA[1][0],axis=0)
-    std_groupA_22 = np.std(groupA[1][1],axis=0)
-    std_groupB_11 = np.std(groupB[0][0],axis=0)
-    std_groupB_12 = np.std(groupB[0][1],axis=0)
-    std_groupB_21 = np.std(groupB[1][0],axis=0)
-    std_groupB_22 = np.std(groupB[1][1],axis=0)
-
-    data_mean = np.array([[mean_groupA_11[0],mean_groupA_21[0],mean_groupB_11[0],mean_groupB_21[0]],
-                            [mean_groupA_11[1],mean_groupA_21[1],mean_groupB_11[1],mean_groupB_21[1]],
-                            [mean_groupA_11[2],mean_groupA_21[2],mean_groupB_11[2],mean_groupB_21[2]],
-                            [mean_groupA_11[3],mean_groupA_21[3],mean_groupB_11[3],mean_groupB_21[3]],
-                            [mean_groupA_12[0],mean_groupA_22[0],mean_groupB_12[0],mean_groupB_22[0]],
-                            [mean_groupA_12[1],mean_groupA_22[1],mean_groupB_12[1],mean_groupB_22[1]],
-                            [mean_groupA_12[2],mean_groupA_22[2],mean_groupB_12[2],mean_groupB_22[2]],
-                            [mean_groupA_12[3],mean_groupA_22[3],mean_groupB_12[3],mean_groupB_22[3]]])
-
-    data_std = np.array([[std_groupA_11[0],std_groupA_21[0],std_groupB_11[0],std_groupB_21[0]],
-                            [std_groupA_11[1],std_groupA_21[1],std_groupB_11[1],std_groupB_21[1]],
-                            [std_groupA_11[2],std_groupA_21[2],std_groupB_11[2],std_groupB_21[2]],
-                            [std_groupA_11[3],std_groupA_21[3],std_groupB_11[3],std_groupB_21[3]],
-                            [std_groupA_12[0],std_groupA_22[0],std_groupB_12[0],std_groupB_22[0]],
-                            [std_groupA_12[1],std_groupA_22[1],std_groupB_12[1],std_groupB_22[1]],
-                            [std_groupA_12[2],std_groupA_22[2],std_groupB_12[2],std_groupB_22[2]],
-                            [std_groupA_12[3],std_groupA_22[3],std_groupB_12[3],std_groupB_22[3]]])
-
-
-
-    length = len(data_mean)
-
-    # Set plot parameters
-    fig, ax = plt.subplots(figsize=(figure_size[0],figure_size[1]))
-    width = 0.2 # width of bar
-    x = np.arange(length)
-
-    ax.bar(x, data_mean[:,0], width, color='#90EE90', label=groups[0], yerr=data_std[:,0])
-    ax.bar(x + width, data_mean[:,1], width, color='#013220', label=groups[1], yerr=data_std[:,1])
-    ax.bar(x + (2 * width), data_mean[:,2], width, color='#C4A484', label=groups[2], yerr=data_std[:,2])
-    ax.bar(x + (3 * width), data_mean[:,3], width, color='#654321', label=groups[3], yerr=data_std[:,3])
-
-    ax.set_ylabel('Average Band Power')
-    #ax.set_ylim(0,1000)
-    #ax.set_yticks(np.arange(0,1000,100))
-    ax.set_xticks(x + width + width/2)
-    ax.set_xticklabels(x_labels)
-    ax.set_title(plot_title)
-    ax.legend()
-    plt.grid(True, 'major', 'y', ls='--', lw=.5, c='k', alpha=.3)
-
-    fig.tight_layout()
-    plt.show()
-    pass
+        avg_BandPower.append(absPower(data[:,i],fs,low,high))
+    avg_BandPower= np.array(avg_BandPower).T
+    return avg_BandPower
 
 def plots(x,y,titles,pltclr):
     x_lim = [x[0],x[-1]]
@@ -305,75 +201,6 @@ def rollingWindow(array,window_size,freq):
     out_final = np.asarray(out_final).T
     out_final = out_final.transpose()
     return out_final
-
-def customICA(input,tuneVal):
-    # algorithm uses ICA to split the eeg into components to allow for easy extraction of ocular artefacts
-    # Using the idea of the ASR algorithm, the tuneVal is the number of STD above the mean
-    # eeg elements that fall into the class above the mean are artefacts and replaced with 
-    # the mean value of non-artefact elements
-    val = tuneVal
-    ica = FastICA(len(input.T),max_iter=200,tol=0.0001,random_state=0)
-    components = ica.fit_transform(input)  
-
-    def fixICAcomps(datae,val):
-        mean = np.mean(datae, axis=0)
-        sd = np.std(datae, axis=0)
-        final_list = [x for x in datae if (x > mean - val * sd)]
-        final_list = [x for x in final_list if (x < mean + val * sd)]
-        final_list = np.asarray(final_list)
-
-        def returnNotMatches(a, b):
-            a = set(a)
-            b = set(b)
-            return [list(b - a), list(a - b)]
-
-        rejected = np.asarray(returnNotMatches(datae,final_list)[1])
-        rejected = rejected.reshape(len(rejected),1)
-        idx = np.where(datae==rejected)[1]
-        idx = idx.tolist()
-        #idx = idx.reshape(len(idx),1)
-        #datae = datae.reshape(len(datae),1)
-        replace_vals = [np.mean(final_list)] * len(idx)
-        fixedComps = [replace_vals[idx.index(i)] if i in idx else datae[i] for i in range(len(datae))]
-        return fixedComps
-
-    out_final = []
-    for i in range(len(components.T)):
-        out_init = fixICAcomps(components[:,i],val)
-        out_final.append(out_init)
-    out_final = np.asarray(out_final).T
-    x_restored = ica.inverse_transform(out_final)
-    return x_restored
-
-def averageBandPower(data,arrayType,fs,low,high,win):
-    #  Inputs  :   data    - 2D numpy array (d0 = samples, d1 = channels) of filtered EEG data
-    #              or data - 3D numpy array (d0 = channels, d1 = no of windows, d2 = length of windows) of unfiltered EEG data
-    #              arrayType - '2D' or '3D'
-    #              fs      - sampling rate of hardware (defaults to config)
-    #              low     - lower limit in Hz for the brain wave
-    #              high    - upper limit in Hz for the brain wave
-    #              win     - size of window to be used for sliding
-    #   Output  :   3D array (columns of array,no of windows,window size)
-    def absPower(data,fs,low,high,win):                                                 
-        freqs, psd = signal.welch(data,fs,nperseg=win)
-        idx_freqBands = np.logical_and(freqs >= low, freqs <= high) 
-        freq_res = freqs[1] - freqs[0]                                  
-        freqBand_power = simps(psd[idx_freqBands],dx=freq_res)  
-        return freqBand_power
-    if arrayType=='2D':
-        avgBandPower = []
-        for i in range(len(data.T)):
-            avgBandPower.append(absPower(data[:,i],fs,low,high,win))
-        avgBandPower= np.array(avgBandPower).T
-    elif arrayType=='3D':
-        avgBandPower = []
-        for i in range(len(data)):
-            x = data[i,:,:]
-            for i in range(len(x)):
-                avgBandPower.append(absPower(x[i,:],fs,low,high,win))
-        avgBandPower= np.array(avgBandPower)
-        avgBandPower = avgBandPower.reshape(len(x),len(data))
-    return avgBandPower
 
 def spectogramPlot(data,fs,nfft,nOverlap,figsize,subTitles,title):
     #   Inputs  :   data    - 2D numpy array (d0 = samples, d1 = channels) of filtered EEG data
@@ -506,86 +333,6 @@ def statTest(test_type,data_1,data_2,show_output,variableName,channelName,alpha=
         stat_test_3 = np.array(stat_test_3).T
         print("\n")
     return stat_test_3
-
-def singleChannelDWT(data,time_array,wavelet):
-    #   Probability Mapping Based Artifact Detection and Wavelet Denoising based 
-    #   Artifact Removal from Scalp EEG for BCI Applications
-    #  Perform DWT on the data
-    #   Input: data - EEG data: 1D array (samples x channel)
-    #   Output: new signal: (samples x number of wavelets)
-    #           signal_global - new signal extracted after global threshold 
-    #           signal_std - new signal extracted after std threshold 
-    #   Reference:  choice of number of levels to threshold gotten from "Comparative Study of Wavelet-Based Unsupervised 
-    #               Ocular Artifact Removal Techniques for Single-Channel EEG Data"
-    
-    def dwt_only(data,wavelet):
-        def dwt(data,wavelet):
-            coeffs = wavedec(data,wavelet,level=10)
-            return np.array(coeffs,dtype=object).T
-
-        def global_threshold(data,coeffs):
-            def coeffs_approx(data,coeffs):
-                return (np.median(abs(coeffs[0]))/0.6745)*(np.sqrt(2*np.log(len(data))))
-            def coeffs_detail(data,coeffs):
-                return (np.median(abs(coeffs[1]))/0.6745)*(np.sqrt(2*np.log(len(data))))
-            arr_approx = coeffs_approx(data,coeffs)
-            arr_detail = coeffs_detail(data,coeffs)
-            return np.vstack((arr_approx,arr_detail))
-
-        def apply_threshold(coeffs,threshold):
-            def apply_threshold_approx(coeffs,threshold):
-                #coeffs[0][abs(coeffs[0])>threshold[1]] = 0
-                #coeffs_approx = coeffs[0]
-                coeffs_approx = np.zeros(len(coeffs[0]))
-                return coeffs_approx
-            def apply_threshold_detail(coeffs,threshold):
-                coeffs = coeffs[1:len(coeffs)]
-                coeffs[0][abs(coeffs[0])>threshold[1]] = 0
-                coeffs[1][abs(coeffs[1])>threshold[1]] = 0
-                coeffs[2][abs(coeffs[2])>threshold[1]] = 0  # level 8
-                coeffs[3][abs(coeffs[3])>threshold[1]] = 0  # level 7
-                coeffs[4][abs(coeffs[4])>threshold[1]] = 0  # level 6
-                coeffs[5][abs(coeffs[5])>threshold[1]] = 0  # level 5
-                coeffs[6][abs(coeffs[6])>threshold[1]] = 0  # level 4
-                coeffs[7][abs(coeffs[7])>threshold[1]] = 0  # level 3
-                coeffs[8][abs(coeffs[8])>threshold[1]] = 0
-                coeffs[9][abs(coeffs[9])>threshold[1]] = 0
-                return coeffs
-            arr_approx = apply_threshold_approx(coeffs,threshold)
-            arr_detail = apply_threshold_detail(coeffs,threshold)
-            arr_detail = list(np.array(arr_detail).T)
-            arr_approx = arr_approx
-            coefs = arr_detail
-            (coefs).insert(0,arr_approx)
-            return coefs
-
-        def inv_dwt(coeffs,wavelet):
-            def inverse_dwt(coeffs,wavelet):
-                return waverec(coeffs,wavelet)
-            arr = (inverse_dwt(list(np.array(coeffs,dtype=object)),wavelet))
-            return  (np.array(arr).T)[:-1]
-
-        coeffs = dwt(data,wavelet)
-        threshold_global = global_threshold(data,coeffs)
-        coeffs_global = apply_threshold(coeffs,threshold_global)
-        signal_global = inv_dwt(coeffs_global,wavelet)
-        return signal_global
-
-    newEEG_global = []
-    for i in range(len(wavelet)):
-        newEEG_global.append((dwt_only(data,wavelet[i])))
-    newEEG_global = np.array(newEEG_global).T
-    if len(newEEG_global) != len(time_array):
-        if len(newEEG_global) > len(time_array):
-            diff = len(newEEG_global) - len(time_array)
-            newEEG_global = newEEG_global[:-diff,:]
-        elif len(newEEG_global) < len(time_array):
-            diff = len(time_array) - len(newEEG_global)
-            num_zeros = np.zeros((diff,len(newEEG_global[1])))
-            newEEG_global = np.append(newEEG_global,num_zeros,axis=0)
-    else:
-        newEEG_global = newEEG_global
-    return newEEG_global
 
 def psdPlots(data,fs,titles):
 # Define window length (4 seconds)
